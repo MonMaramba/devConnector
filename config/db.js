e7812268b7285b729c6156b57ea7f43dc6760ef1
MonMaramba <monmaramba@yahoo.com>
2020-11-21 08:11:03 -0800

    check for user email, hashing pw and pulling avatar

--- config/db.js
+++ config/db.js
@@ -3,18 +3,19 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongooose.connect(db, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+  try {
+    await mongooose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
 
-        console.log('MongDB connected...')
-    } catch(err){
-        console.error(err.message);
-        //Exit process with failure
-        process.exit(1)
-    }
+    console.log('MongDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,17 +1,59 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator')
+const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 
-// @route POST api/users
+// @route  POST api/users
 // @desc   Register user
 // @access Public
-router.post('/', [
-    check('name', "Name is required").not().isEmpty(), check('email', 'Please include a valid email').isEmail(), check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
-], (req, res) => {
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array()})
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route')});
+    const { name, email, password } = req.body;
+    try {
+      //checking for the email so that only one user per email is registered
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ erros: [{ msg: 'User already exists' }] });
+      }
+      // getting the gravatar
+      const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
+      // setting an instance of the user using the model
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+      // hashing the password before saving
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
